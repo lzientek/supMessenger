84b76b28a8035ed53c22aec2c57646fd70f9843c
lzientek <152565@supinfo.com>
2014-11-29 01:31:06 +0000

    creation du CRUD pour User

--- src/com/supinfo/supMessaging/dao/UserDao.java
+++ src/com/supinfo/supMessaging/dao/UserDao.java
@@ -1,7 +1,22 @@
 package com.supinfo.supMessaging.dao;
 
+import com.supinfo.supMessaging.entities.User;
+
+import java.util.List;
+
 /**
  * Created on the 28/11/2014.
  */
 public interface UserDao {
+    void addUser(User user);
+
+    User getUserById(Long id);
+
+    User findUserByUsername(String username);
+
+    User getUserByIdWithContacts(Long id);
+
+    List<User> getAllUsers();
+
+    void removeUser(User user);
 }
--- src/com/supinfo/supMessaging/dao/jpa/JpaUserDao.java
+++ src/com/supinfo/supMessaging/dao/jpa/JpaUserDao.java
@@ -1,8 +1,14 @@
 package com.supinfo.supMessaging.dao.jpa;
 
 import com.supinfo.supMessaging.dao.UserDao;
+import com.supinfo.supMessaging.entities.User;
+import com.supinfo.supMessaging.helpers.PersistHelper;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import java.util.List;
 
 /**
  * Created on the 28/11/2014.
@@ -12,5 +18,71 @@ public class JpaUserDao implements UserDao {
 
     public JpaUserDao(EntityManagerFactory entityManagerFactory) {
         emf = entityManagerFactory;
+    }
+
+    @Override
+    public void addUser(User user) {
+        PersistHelper.persist(emf, user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query query = em
+                    .createQuery("SELECT u FROM  User u WHERE u.id = :id");
+            query.setParameter("id", id);
+            return (User) query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public User getUserByIdWithContacts(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query query = em
+                    .createQuery("SELECT u FROM  User u LEFT JOIN FETCH u.contacts WHERE u.id = :id");
+            query.setParameter("id", id);
+            return (User) query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query query = em
+                    .createQuery("SELECT u FROM  User u WHERE u.userName = :username");
+            query.setParameter("username", username);
+            return (User) query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query query = em.createQuery("SELECT u FROM User AS u");
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void removeUser(User user) {
+        PersistHelper.remove(emf, user);
     }
 }
