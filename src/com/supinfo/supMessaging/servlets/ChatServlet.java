a6347c821a4609199277ce72498d4e6f3a7011b6
lzientek <152565@supinfo.com>
2014-12-01 18:09:55 +0000

    envoie des messages fonctionnent

--- src/com/supinfo/supMessaging/servlets/ChatServlet.java
+++ src/com/supinfo/supMessaging/servlets/ChatServlet.java
@@ -19,36 +19,53 @@ import java.util.List;
 @WebServlet("/Chat")
 public class ChatServlet extends HttpServlet {
 
+    private User contact;
+    private User actualUser;
+
 
     private void getValues(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        User actualUser = DaoFactory.getUserDao().getUserByIdWithContacts((Long) req.getSession().getAttribute(Constant.userSession));
-        req.setAttribute("user", actualUser);
-
         //get messages
         try {
-            if (req.getParameter("contactId") != null) {
-                Long contactId = Long.parseLong(req.getParameter("contactId"));
-                User contact = DaoFactory.getUserDao().getUserById(contactId);
-                if (contact == null) {
-                    throw new Exception("Invalide contact id");
-                }
+
+            if (contact != null) {
                 List<Message> messages = DaoFactory.getMessageDao().getMessagesByUsers(actualUser, contact);
                 if (messages.size() == 0) {
                     throw new Exception("No Message");
                 }
                 req.setAttribute("messages", messages);
-            }
+                }
 
         } catch (Exception ex) {
             req.setAttribute("error", ex.getMessage());
         }
     }
 
+    private void getUsers(HttpServletRequest req) throws Exception {
+        actualUser = DaoFactory.getUserDao().getUserByIdWithContacts((Long) req.getSession().getAttribute(Constant.userSession));
+        req.setAttribute("user", actualUser);
+
+        if (req.getParameter("contactId") != null) {
+            Long contactId = Long.parseLong(req.getParameter("contactId"));
+            contact = DaoFactory.getUserDao().getUserById(contactId);
+            if (contact == null) {
+                throw new Exception("Invalide contact id");
+            }
+        } else {
+            throw new Exception("Select a contact");
+        }
+
+    }
+
     /**
      * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            getUsers(request);
+        } catch (Exception e) {
+            request.setAttribute("error", e.getMessage());
+        }
 
         getValues(request, response);
         request.getRequestDispatcher("Chat.jsp").forward(request, response);
@@ -58,7 +75,23 @@ public class ChatServlet extends HttpServlet {
      * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            getUsers(request);
+            String msg = request.getParameter("message");
+            if (msg == null || msg.trim().isEmpty()) {
+                throw new Exception("Empty message!");
+            }
+            Message message = new Message();
+            message.setMessage(msg);
+            message.setRecipient(contact);
+            message.setTransmitter(actualUser);
+            DaoFactory.getMessageDao().sendMessage(message);
+        } catch (Exception e) {
+            request.setAttribute("sendError", e.getMessage());
+        }
+
         getValues(request, response);
+
         request.getRequestDispatcher("Chat.jsp").forward(request, response);
     }
 
