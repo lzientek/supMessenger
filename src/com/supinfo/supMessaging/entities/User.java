b4377886321a6dee9923c209963e95d8bc5a7a2a
lzientek <152565@supinfo.com>
2014-12-10 15:17:15 +0000

    commit résolution du probleme isAdmin

--- src/com/supinfo/supMessaging/entities/User.java
+++ src/com/supinfo/supMessaging/entities/User.java
@@ -76,7 +76,7 @@ public class User implements Serializable {
     }
 
     public Boolean getIsAdmin() {
-        return isAdmin;
+        return isAdmin != null && isAdmin;
     }
 
     public void setIsAdmin(Boolean isAdmin) {
